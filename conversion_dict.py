fd0a24022373885d0f5f0aef782be995f613e9a1
Amy Weng <amycweng@gmail.com>
2022-06-01 15:28:27 -0400

    add citations

--- conversion_dict.py
+++ conversion_dict.py
@@ -1,3 +1,31 @@
+"""
+BSD 2-Clause License
+
+Copyright (c) 2017, Authors who have agreed to assign their rights to 
+The University of Wisconsin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 def getBrevigraphList():
     brevs = ['CON','bus','con', 'er','is','PER','per','pre','pri','PRI','PRO','pro','qu','quam','que','QUE','qui','QUOD','quod','ris','rum','RUM','sed','ser','ur','us','US','es','etc','ou','precipi','subli','ignifi']
     shorthand = ['THAT','that','half','ducat','quarter','hundred','aqua fortis','aqua regis','air','alembic','alum','antimony','aqua regis','arsenic','ashes','quicklime','potash','query','vitriol','abl.','vinegar','sulfur','fire','earth','glass','tartar','talc']
