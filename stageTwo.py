f5c0f0220566d1e507ae3e8134bb7c515372a52b
joy <shijia.tong@duke.edu>
2022-06-09 15:23:18 -0400

    commit again

--- stageTwo.py
+++ stageTwo.py
@@ -9,5 +9,3 @@ STAGE II FUNCTIONS
 '''
 def avgDate(df):
     df['date'] = df['date'].apply(lambda x: round(np.mean([int(y) for y in re.findall(r'\d{4}', x)])) if type(x)==str and re.search('[0-9]+',x) != None else x)
-
-#hello
