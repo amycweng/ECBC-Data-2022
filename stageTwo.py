161a00cca36a888929b92c93c09ddc81af426eaa
joy <shijia.tong@duke.edu>
2022-06-09 15:22:36 -0400

    try

--- stageTwo.py
+++ stageTwo.py
@@ -10,3 +10,4 @@ STAGE II FUNCTIONS
 def avgDate(df):
     df['date'] = df['date'].apply(lambda x: round(np.mean([int(y) for y in re.findall(r'\d{4}', x)])) if type(x)==str and re.search('[0-9]+',x) != None else x)
 
+#hello
