e5695ca96b9bd01736be09c93217ed7873e3ff1f
Yirui Zhao <yz535@research-datap-ec-01.oit.duke.edu>
2022-06-27 11:42:51 -0400

    add words with more than 600 freq

--- stageTwo/cryMore3.py
+++ stageTwo/cryMore3.py
@@ -1,12 +1,24 @@
-places = ['banda', 'plymouth', 'poland', 'venetia', 'scotland', 
+geographic = ['banda', 'plymouth', 'poland', 'venetia', 'scotland', 
 'denmark', 'damascus', 'guinea', 'paris', 'cyprus', 'powhatan', 
 'bengala', 'britain', 'prussia', 'agra', 'malacca', 'cuba', 'antwerp', 
 'sumatra', 'hispaniola', 'moha','aethiopia', 'havana', 'pechora', 'venetian',
  'seville', 'moscovia', 'cibola', 'oxford', 'provence', 'castro',
  'livonia', 'soldania', 'paracoussy', 'cataia', 'morea', 'biscay','muscovite',
- 'johannes','sanderson','nicolo']
+ 'portugal', 'spaniard', 'england', 'spain', 'india', 'tartar', 'mexico', 
+ 'france', 'russia', 'venice', 'peru', 'persia', 'egypt', 'jerusalem',
+ 'america', 'constantinople', 'rial', 'flanders', 'cochin', 'cananor', 'arica',
+ 'zeila', 'camden', 'assyria', 'borneo', 'east-indies', 'hudson', 'yeraslave',
+ 'galicia']
 
-confuse =['walter', 'tercera', 'capac' ]
+people = ['johannes','sanderson','nicolo', 'john', 'thomas', 'william',
+'henry','richard', 'hollander', 'francis', 'elizabeth', 'antonio', 
+'sebastian', 'andrew', 'abraham', 'francisco', 'joseph', 'lewis', 'hawkins',
+'laurence', 'matthew', 'diego', 'janni', 'augustine', 'reys', 'arias',
+'pheodorowich', 'isabel', 'josephus', 'sio', 'sarmiento', 'rawlins',
+'anna', 'vasilowich', 'selim', 'cantan', 'mendoça' ]
+
+confuse =['walter', 'tercera', 'capac', 'cocrk', 'cola', 'sabandar',
+'vandal', 'amacao' ]
 
 lemmas = {'januarie':'january',
           'ganges':'gange',
@@ -14,5 +26,11 @@ lemmas = {'januarie':'january',
           'curran':'currant',
           'pengwin':'pinguin',
           'pinnasses':'pinnace',
-          'cananor':'kannur'
-          }
+          'cananor':'kannur',
+          'straits':'strait',
+          'clothes':'clothe', #Not sure
+          'francis':'francisco', #Maybe?
+          'yeraslave':'yaroslavl', #Not sure
+          'cantar':'kantar',
+          'maronites':'maronite'
+}
